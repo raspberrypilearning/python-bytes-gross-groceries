7ab286c61e7fe930d3accac8e3fa8d47a2223efe
Marc Scott <marc@raspberrypi.org>
2025-04-14 16:11:25 +0100

    update

--- en/solutions/main.py
+++ en/solutions/main.py
@@ -1,6 +1,7 @@
 # Gross Groceries recipe card
 
 # Ingredients and amounts for 1 person
+
 ingredient_1 = 'maggot mash 🐛'
 amount_1 = 50
 
@@ -9,7 +10,6 @@ amount_2 = 30
 
 ingredient_3 = 'toenail sprinkles 🦶'
 amount_3 = 10
-
 # Number of servings
 servings = 3
 
@@ -19,6 +19,9 @@ print(f'{amount_2 * servings}ml of {ingredient_2}')
 print(f'{amount_3 * servings}g of {ingredient_3}')
 
 # Print recipe instructions
-print(f'To make this revolting recipe for {servings} people, mix {ingredient_1}, {ingredient_2}, and {ingredient_3} in a large bowl.')
-print(f'Add {amount_1 * servings}g of {ingredient_1} and pour over {amount_2 * servings}ml of {ingredient_2}.')
-print(f'Sprinkle with {amount_3 * servings}g of {ingredient_3} and serve cold. Yum!')
+print()
+print(f'To make this revolting recipe for {servings} people add {amount_1 * servings}g of {ingredient_1} to a large bowl.')
+print()
+print(f'Pour over {amount_2 * servings}ml of {ingredient_2}.')
+print()
+print(f'Sprinkle with {amount_3 * servings}g of {ingredient_3} and serve cold 🧊. Yum!')
